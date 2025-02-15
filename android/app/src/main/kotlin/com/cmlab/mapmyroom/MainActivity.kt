f54b56a407163785a7dde109974dc4f336dd3c17
Mukund Lal <mukundlaltnr@gmail.com>
2025-02-16 00:03:34 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/cmlab/mapmyroom/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.cmlab.mapmyroom
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MapMyRoomApp());
+}
+class MapMyRoomApp extends StatelessWidget {
+  const MapMyRoomApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
